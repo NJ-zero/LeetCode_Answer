7d751558c85e9d83d6c9d624e2c5657dfa0a98b9
NJ-Zero <821854810@qq.com>
2018-08-23 21:03:04 +0800

    length Of LastWord

---
+++ lengthOfLastWord.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+# author='Shichao-Dong'
+# create time: 2018/8/23
+'''
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+如果不存在最后一个单词，请返回 0 。
+输入: "Hello World"
+输出: 5
+"   a  "
+1
+'''
+
+class solution():
+    def lengthOfLastWord(self,s):
+        return (len(s.strip().split(" ")[-1]))
+
+if __name__=="__main__":
+    s=solution()
+    ss=" hello world "
+    a=s.lengthOfLastWord(ss)
+    print(a)
