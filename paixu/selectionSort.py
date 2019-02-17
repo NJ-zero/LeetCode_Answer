eb040c947de0dafd51a05dbf369c65f93258fae7
dongshichao <821854810@qq.com>
2019-02-17 22:43:43 +0800

    select sort

--- paixu/selectionSort.py
+++ paixu/selectionSort.py
@@ -14,3 +14,17 @@ def selectsort(a):
     print(a)
 a=[2,1,3,5,12,15,34,23]
 selectsort(a)
+
+
+def find(arr):
+    for i in range(len(arr)-1):
+        small=arr[i]
+        for j in range(i+1,len(arr)):
+            if arr[j]<small:
+                tmp=small
+                small=arr[j]
+                arr[j]=tmp
+        arr[i]=small
+    print(arr)
+
+find([1,333,5,12,14,23,24,36,11,10,9])
