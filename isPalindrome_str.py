d2f428dda39bf266e9858520ba4c12df39bb0757
NJ-Zero <821854810@qq.com>
2019-03-06 16:58:36 +0800

    检查回文数

---
+++ isPalindrome_str.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# author='Shichao-Dong'
+# create time: 2019/3/6 
+
+import re
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s=re.sub('[^a-z0-9]','',s.lower())
+        print(s)
+        return s==s[::-1]
+
+if __name__=='__main__':
+    s=Solution()
+    a=s.isPalindrome("A man, a plan, a canal: Panama")
