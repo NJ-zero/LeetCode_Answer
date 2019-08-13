de5d1a9428ab7e8d30cc94244a0055fd487455bf
dongshichao <dongshichao@qutoutiao.net>
2019-08-13 14:24:15 +0800

    对撞指针

--- isPalindrome_str.py
+++ isPalindrome_str.py
@@ -15,7 +15,30 @@ class Solution(object):
         s=re.sub('[^a-z0-9]','',s.lower())
         print(s)
         return s==s[::-1]
+    def is_s(self,s):
+        s= ''.join(filter(str.isalnum(),s)).lower()
+        return s==s[::-1]
+
+    def is_dd(self,s):
+        n= len(s)
+        i,j = 0,n-1
+        while i < j:
+            if s[i].isalnum() == False:
+                i+=1
+                continue
+            if s[j].isalnum() == False:
+                j -=1
+                continue
+            if s[i].lower() != s[j].lower():
+                return False
+            i+=1
+            j-=1
+        return True
 
 if __name__=='__main__':
     s=Solution()
     a=s.isPalindrome("A man, a plan, a canal: Panama")
+    print(a)
+
+    a = s.is_dd("A man, a plan, a canal: Panama")
+    print(a)
--- reverseVowels.py
+++ reverseVowels.py
@@ -24,9 +24,31 @@ class Solution(object):
                 s_list[i] = vo_s.pop()
         return ''.join(s_list)
 
+    def vers(self,s):
+        ss = list(s)
+        n = len(s)
+        i ,j = 0, n-1
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        while i<j :
+            if ss[j] not in vowels:
+                j -= 1
+                continue
+            if ss[i] not in vowels:
+                i += 1
+                continue
+
+            ss[i],ss[j] = ss[j],ss[i]
+            i += 1
+            j -= 1
+
+        return "".join(ss)
+
+
 if __name__=="__main__":
     s=Solution()
     ss=s.reverseVowels("leetcode")
     print(ss)
     a="aassddccx"
     print(a[::-1])
+
+    print(s.vers("leetcode"))
