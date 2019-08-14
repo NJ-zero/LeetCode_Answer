d31f1e95ac6bea4d018ad321f0f2d0ce013dca5f
dongshichao <dongshichao@qutoutiao.net>
2019-08-14 11:35:47 +0800

    移除指定元素

--- removeelements.py
+++ removeelements.py
@@ -21,8 +21,19 @@ class Solution():
             i+=1
         print (nums)
 
+    def removee(self,nums,val):
+        n = len(nums)
+        i ,j = 0,0
+        while i <n:
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j+=1
+            i+=1
+        print(nums)
+
 if __name__=='__main__':
     s=Solution()
     nums=[1,2,3,4,5,1,3,7,4,3,2]
     val=2
     s.removeElement(nums,val)
+    s.removee(nums,val)
