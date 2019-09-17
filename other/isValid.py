e79218c40d5e5780b63900ee5bebf99cb5c92f99
dongshichao <dongshichao@qutoutiao.net>
2019-09-17 16:17:20 +0800

    20.有效的括号

--- other/isValid.py
+++ other/isValid.py
@@ -15,6 +15,14 @@
 输出: true
 输入: "([)]"
 输出: false
+
+
+解题思路：
+每组括号，组成一对 k v
+遍历字符串，如果i 在 k中，就加到列表中
+如果不在k中，肯定就是v,且这个v 应该和。list最后一个元素相等
+
+理想情况，最终的list为空
 '''
 class Solution(object):
     def isValid(self, s):
@@ -35,7 +43,26 @@ class Solution(object):
                     return False
         return l == []
 
+    def isValid2(self, s):
+        all = {'{': '}', '[': ']', '(': ')'}
+        l = []
+
+        if len(s) % 2 == 1:
+            return False
+        if len(s) == 0:
+            return True
+        if s[0] not in all:
+            return False
+        for i in s:
+            if i in all:
+                l.append(all[i])
+                print(l)
+            else:
+                if not l or l.pop() != i:
+                    return False
+        return l == []
+
 if __name__=='__main__':
     s=Solution()
-    ss=""
+    ss="{}[]([])"
     print(s.isValid(ss))
