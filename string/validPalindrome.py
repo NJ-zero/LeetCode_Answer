a6498ce1ee907387c9998ede9ae90d4f27fa2863
dongshichao <dongshichao@qutoutiao.net>
2019-10-16 17:38:52 +0800

    680. 验证回文字符串 Ⅱ

--- string/validPalindrome.py
+++ string/validPalindrome.py
@@ -5,6 +5,10 @@
 
 '''
 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串
+
+双指针
+遇到不同的元素，判断去掉这个元素能不能成为回文字符
+
 '''
 class Solution(object):
     def validPalindrome(self, s):
@@ -33,6 +37,19 @@ class Solution(object):
             left+=1
             right-=1
         return True
+
+    def checc(self,s):
+        ispand = lambda s : s == s[-1]
+        sp = lambda s,x : s[:x] + s[x+1:]
+        i,j = 0,len(s)-1
+        while i < j :
+            if s[i] != s[j]:
+                return ispand(sp(s,i)) or ispand(sp(s,j))
+            i +=1
+            j -=1
+        return True
+
+
 
 if __name__=="__main__":
     s=Solution()
