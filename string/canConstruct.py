ec4e98a45392ff55d5e3c33a948ec494082aff28
dongshichao <dongshichao@qutoutiao.net>
2019-10-17 16:50:02 +0800

    383.赎金信

---
+++ string/canConstruct.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+# Time: 2019-10-17-16:33 
+# Author: dongshichao
+
+'''
+给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。
+如果可以构成，返回 true ；否则返回 false。
+
+(题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。)
+
+注意：
+
+你可以假设两个字符串均只含有小写字母。
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+
+'''
+
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        for i in ransomNote:
+            if i not in magazine:
+                return False
+            else:
+                index = magazine.index(i)
+                magazine = magazine[:index]+magazine[index+1:]
+        return True
+
+s=Solution()
+print(s.canConstruct("aa","aba"))
