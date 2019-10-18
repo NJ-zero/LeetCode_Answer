064a5a3b209d3bfc118dfb82663639100e1e2717
dongshichao <dongshichao@qutoutiao.net>
2019-10-18 19:05:03 +0800

    521. 最长特殊序列 Ⅰ

---
+++ string/findLUSlength.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+# Time: 2019-10-18-19:03 
+# Author: dongshichao
+
+'''
+给定两个字符串，你需要从这两个字符串中找出最长的特殊序列。最长特殊序列定义如下：该序列为某字符串独有的最长子序列（即不能是其他字符串的子序列）。
+
+子序列可以通过删去字符串中的某些字符实现，但不能改变剩余字符的相对顺序。空序列为所有字符串的子序列，任何字符串为其自身的子序列。
+
+输入为两个字符串，输出最长特殊序列的长度。如果不存在，则返回 -1。
+
+示例 :
+
+输入: "aba", "cdc"
+输出: 3
+解析: 最长特殊序列可为 "aba" (或 "cdc")
+
+三种场景：
+1。a = b 返回-1
+2。a !=b 返回较长的那个，长度一样，则返回人任意一个长度
+'''
+
+
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        if a == b:
+            return -1
+        else:
+            if len(a) != len(b):
+                return max(len(a),len(b))
+            else:
+                return len(a)
