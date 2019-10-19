96053c2516f16a731aaba1bb0e757440c2942d7b
dongshichao <dongshichao@qutoutiao.net>
2019-10-19 17:21:45 +0800

    925. 长按键入

---
+++ string/isLongPressedName.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+# Time: 2019-10-19-17:15 
+# Author: dongshichao
+
+
+'''
+你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
+
+你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True。
+
+
+示例 1：
+
+输入：name = "alex", typed = "aaleex"
+输出：true
+解释：'alex' 中的 'a' 和 'e' 被长按。
+示例 2：
+
+输入：name = "saeed", typed = "ssaaedd"
+输出：false
+解释：'e' 一定需要被键入两次，但在 typed 的输出中不是这样。
+示例 3：
+
+输入：name = "leelee", typed = "lleeelee"
+输出：true
+示例 4：
+
+输入：name = "laiden", typed = "laiden"
+输出：true
+解释：长按名字中的字符并不是必要的。
+
+'''
+
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        """
+        :type name: str
+        :type typed: str
+        :rtype: bool
+        """
+        pos = 0
+        i=0
+        while i < len(typed):
+            if typed[i] == name[pos]:
+                print(name[pos],pos)
+                pos += 1
+            if pos > len(name)-1:
+                break
+            i +=1
+
+        return pos == len(name)
+
+s= Solution()
+print(s.isLongPressedName("aleeexx","aaleeeexxx"))
+
+a = "abc"
+print(a[0])
