8eb09cc1c79f8ed0c742baed747f02d8b0bd4bca
dongshichao <dongshichao@qutoutiao.net>
2019-10-22 10:09:30 +0800

    19. 删除链表的倒数第N个节点

--- lianbiao/removeNthFromEnd.py
+++ lianbiao/removeNthFromEnd.py
@@ -3,6 +3,21 @@
 # Author: dongshichao
 
 
+'''
+给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+
+示例：
+
+给定一个链表: 1->2->3->4->5, 和 n = 2.
+
+当删除了倒数第二个节点后，链表变为 1->2->3->5
+
+思路：
+快慢指针，快指针先走n步
+快指针到末尾，满指针停留在倒数 n 个节点
+'''
+
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
