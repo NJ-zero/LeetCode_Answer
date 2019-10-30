4e4745da32b536177709e4ea37518757d2347dd6
dongshichao <dongshichao@qutoutiao.net>
2019-10-30 20:26:55 +0800

    475.供暖器

---
+++ list/findRadius.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+# Time: 2019-10-30-20:13 
+# Author: dongshichao
+
+
+
+class Solution(object):
+    def findRadius(self, houses, heaters):
+        """
+        :type houses: List[int]
+        :type heaters: List[int]
+        :rtype: int
+        """
+        houses.sort()
+        heaters.append(float("inf")) # 保证最终index不会越界
+        heaters.sort()
+        max_dist,index=0,0
+        for house in houses:
+            while house >= heaters[index]:
+                index += 1
+            if index >0 :
+                curr = min(heaters[index]-house,house-heaters[index-1])
+            else:
+                curr = abs(heaters[index]-house)
+            max_dist = max(max_dist,curr)
+        return max_dist
