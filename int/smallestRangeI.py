a22051c3f68c87ef07e3e21a13ac6d680291ee6e
dongshichao <dongshichao@qutoutiao.net>
2019-11-08 17:51:02 +0800

    908.最小差值I

---
+++ int/smallestRangeI.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+# Time: 2019-11-08-17:41 
+# Author: dongshichao
+'''
+908.最小差值I
+
+给定一个整数数组 A，对于每个整数 A[i]，我们可以选择任意 x 满足 -K <= x <= K，并将 x 加到 A[i] 中。
+
+在此过程之后，我们得到一些数组 B。
+
+返回 B 的最大值和 B 的最小值之间可能存在的最小差值。
+
+ 
+示例 1：
+
+输入：A = [1], K = 0
+输出：0
+解释：B = [1]
+示例 2：
+
+输入：A = [0,10], K = 2
+输出：6
+解释：B = [2,8]
+示例 3：
+
+输入：A = [1,3,6], K = 3
+输出：0
+解释：B = [3,3,3] 或 B = [4,4,4]
+
+'''
+class Solution(object):
+    def smallestRangeI(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        l = max(A)
+        r = min(A)
+        if l-r <= 2* K:
+            return 0
+        else:
+            return l-r-2*K
+
+
+s=Solution()
+print(s.smallestRangeI([1,10],2))
+
