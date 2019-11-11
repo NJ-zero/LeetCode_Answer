9be535393c31bc110f35109843f9f698d02b1ddb
dongshichao <dongshichao@qutoutiao.net>
2019-11-11 19:43:14 +0800

    258. 各位相加

---
+++ int/addDigits.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# Time: 2019-11-11-19:33 
+# Author: dongshichao
+
+'''
+258.各位相加
+
+给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。
+
+示例:
+
+输入: 38
+输出: 2
+解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+
+
+'''
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        s = str(num)
+        while len(s) !=1:
+            s=sum([int(i) for i in s])
+            s=str(s)
+        return s
+
+s=Solution()
+print(s.addDigits(38))
