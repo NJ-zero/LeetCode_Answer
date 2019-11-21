a4faab306b0254bf66f920b362636d72663539cf
dongshichao <dongshichao@qutoutiao.net>
2019-11-21 16:27:08 +0800

    198.打家劫舍

--- list/rob.py
+++ list/rob.py
@@ -4,6 +4,7 @@
 
 
 '''
+198.打家劫舍
 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
 
 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
@@ -45,7 +46,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) < 0:
+        if len(nums) <= 0:
             return 0
         if len(nums) == 1:
             return nums[0]
@@ -54,7 +55,7 @@ class Solution(object):
 
         else:
             s1 = self.rob(nums[:-1])
-            s2 = self.rob(nums[-2])
+            s2 = self.rob(nums[:-2])
             return max(s1,s2+nums[-1])
 
     def rob2(self,nums):
