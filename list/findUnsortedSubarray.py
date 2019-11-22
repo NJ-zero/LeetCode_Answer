fe808cd08e5bdfb442263aaa2ac6a295465103b7
dongshichao <dongshichao@qutoutiao.net>
2019-11-22 15:24:46 +0800

    581. 最短无序连续子数组

--- list/findUnsortedSubarray.py
+++ list/findUnsortedSubarray.py
@@ -3,6 +3,8 @@
 # Author: dongshichao
 
 '''
+581. 最短无序连续子数组
+
 给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
 
 你找到的子数组应是最短的，请输出它的长度。
@@ -13,9 +15,7 @@
 输出: 5
 解释: 你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
 '''
 
 class Solution(object):
@@ -55,7 +55,31 @@ class Solution(object):
                 k = n-i-1
         return j-k+1
 
+    def ffff(self,nums):
+        '''
+        两次遍历，一次从前往后，一次从后往前
+        :param nums:
+        :return:
+        '''
+        n = len(nums)
+        if n <=1:
+            return 0
 
+        maxval,minval = nums[0] ,nums[-1]
+        right=0
+        for i in range(n):
+            if nums[i] >= maxval:
+                maxval = nums[i]
+            else:
+                right=i
+        left=n
+        for i in range(n)[::-1]:
+            if nums[i] <= minval:
+                minval = nums[i]
+            else:
+                left=i
+
+        return right-left+1
 
 
 s =Solution()
