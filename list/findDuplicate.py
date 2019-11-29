069c5b8586db93621b1a89b12166533295acbced
dongshichao <dongshichao@qutoutiao.net>
2019-11-29 19:04:53 +0800

    287.寻找重复数

--- list/findDuplicate.py
+++ list/findDuplicate.py
@@ -16,6 +16,16 @@
 输入: [3,1,3,4,2]
 输出: 3
 
+
+二分法思路：
+初试化左右 数字 边界left=1,right=n-1
+循环条件left<right
+mid=(left+right)//2
+按照性质，统计数组中小于等于mid的元素个数count
+若 count<=midc，说明重复数字一定在(mid,right](的范围内。令left=mid+1
+若count>mid，说明重复数字一定在[left,mid]的范围内。令right=mid
+返回left
+
 '''
 
 class Solution(object):
@@ -30,3 +40,19 @@ class Solution(object):
                 dic[i]=1
             else:
                 return i
+
+    def finddd(self,nums):
+        left = 1
+        right = len(nums)-1
+        while left < right:
+            mid = (left+right)//2
+            count = 0
+            for i in nums:
+                if i <= mid:
+                    count +=1
+            if count <= mid:
+                left = mid +1
+            else:
+                right=mid
+
+        return left
