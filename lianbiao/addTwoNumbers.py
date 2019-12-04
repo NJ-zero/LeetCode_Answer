d3c7649b7d9a3601bbcabbf04f517ad64612e6e8
dongshichao <dongshichao@qutoutiao.net>
2019-12-04 19:10:08 +0800

    2.两数相加

---
+++ lianbiao/addTwoNumbers.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+# Time: 2019-12-04-15:51 
+# Author: dongshichao
+
+'''
+2.两数相加
+给出两个 非空 的链表用来表示两个非负的整数。
+其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+
+思路：
+flag用来表示是否有进位，flag非0即1
+
+'''
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        l3 = ListNode(0)
+        cur = l3
+        flag = 0
+        while l1 or l2:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            sum += flag
+            flag = sum //10
+            cur.next = ListNode(sum % 10)
+            cur = cur.next
+        if flag > 0:
+            cur.next = ListNode(1)
+        return l3.next
+
+
+
