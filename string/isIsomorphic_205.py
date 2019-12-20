429ae26d1f0d79ee2fcb87f3bfe0767a5abf65b8
dongshichao <dongshichao@qutoutiao.net>
2019-12-20 14:26:05 +0800

    205.同构字符串

---
+++ string/isIsomorphic_205.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+# Time: 2019-12-20-11:09 
+# Author: dongshichao
+
+'''
+205.同构字符串
+
+给定两个字符串 s 和 t，判断它们是否是同构的。
+
+如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+
+所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+
+示例 1:
+
+输入: s = "egg", t = "add"
+输出: true
+示例 2:
+
+输入: s = "foo", t = "bar"
+输出: false
+示例 3:
+
+输入: s = "paper", t = "title"
+输出: true
+
+
+'''
+
+from collections import Counter
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return list(map(s.index,s))==list(map(t.index,t))
+
+s=Solution()
+print(s.isIsomorphic("abba","abab"))
+
+ss="abab"
+print(list(map(ss.index,ss)))
